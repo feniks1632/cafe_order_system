8fc8b0ffb883c198cc6ede590bf00656b52bfa16
Никита Бражников <feniks1632@gmail.com>
2025-08-12 18:01:27 +0500

    fixed settings

--- cafe_order_system/settings.py
+++ cafe_order_system/settings.py
@@ -21,12 +21,12 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'feniks1632@gmail.com'
-EMAIL_HOST_PASSWORD = 'rwtj bjnt twhl yryy'  # пароль приложения!
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 # Админ, которому приходят уведомления
-ADMIN_EMAIL = 'feniks1632@gmail.com'
+ADMIN_EMAIL = os.getenv('ADMIN_EMAIL')
 
 # Разрешенные хосты
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split()
